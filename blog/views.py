80aec20d940bb22e3ef627a78870c4705a1faf06
Simridhi Sharma <143455521+stressedbleach@users.noreply.github.com>
2024-11-15 18:05:59 +0000

    part 3 - 4

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
+from .forms import BlogPostForm
 
-# Create your views here.
+def post_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+def post_edit(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
